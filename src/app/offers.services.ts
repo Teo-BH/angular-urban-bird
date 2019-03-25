e69e9cc6883974aa7a25fed69e8225e6cba20e1e
Teobaldo Moura <teobaldo.bh@gmail.com>
2019-03-24 21:12:20 -0300

    Add app.api configuration file

--- src/app/offers.services.ts
+++ src/app/offers.services.ts
@@ -1,28 +1,27 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Offer } from './shared/offers.model';
+import { URL_API } from './app.api';
 
 @Injectable()
 export class OffersService {
 
     constructor(private http: HttpClient) { }
 
-    private status = true;
-
     public getOffers(): Promise<Offer[]> {
-        return this.http.get<Offer[]>('http://localhost:3000/ofertas?destaque=true')
+        return this.http.get<Offer[]>(`${URL_API}?destaque=true`)
             .toPromise()
             .then((data) => data);
     }
 
     public getOffersByCategory(category: string): Promise<Offer[]> {
-        return this.http.get<Offer[]>(`http://localhost:3000/ofertas?categoria=${category}`)
+        return this.http.get<Offer[]>(`${URL_API}?categoria=${category}`)
             .toPromise()
             .then((data) => data);
     }
 
     public getOfferById(id: number): Promise<Offer> {
-        return this.http.get<Offer>(`http://localhost:3000/ofertas/${id}`)
+        return this.http.get<Offer>(`${URL_API}/${id}`)
             .toPromise()
             .then((data) => data);
     }
